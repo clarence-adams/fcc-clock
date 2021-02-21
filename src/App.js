69f7e2c873f2aa3652ace6b88b340477ccc6e9c4
Clarence <clarence.e.a@gmail.com>
2021-02-21 21:26:23 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="clock">
+      <BreakButtonGroup/>
+      <SessionButtonGroup/>
+      <div id="clock-wrapper">
+        <p id="timer-label">Session</p>
+        <p id="time-left">25:00</p>
+        <button id="start-stop">>||</button>
+        <button idj="reset">reset</button>
+      </div>
     </div>
-  );
+  )
+}
+
+function BreakButtonGroup() {
+  return (
+    <div id="break-label" class="button-group">
+      <p>Break Length</p>
+      <button id="break-increment">+</button>
+      <p id="break-length">5</p>
+      <button id="break-decrement">-</button>
+    </div>
+  )
+}
+
+function SessionButtonGroup() {
+  return (
+    <div id="session-label" class="button-group">
+      <p>Session Length</p>
+      <button id="session-increment">+</button>
+      <p id="session-length">25</p>
+      <button id="session-decrement">-</button>
+    </div>
+  )
 }
 
 export default App;
