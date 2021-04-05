a82bad827baea3eaba4ea4cf294584374e5428a4
Clarence <clarence.e.a@gmail.com>
2021-04-05 00:11:26 +0000

    Added correct timer formatting and functional increment/decrement buttons

--- src/App.js
+++ src/App.js
@@ -20,6 +20,42 @@ function App() {
     paused ? setPaused(false) : setPaused(true)
   }
 
+  const breakIncrement = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1)
+    }
+  }
+
+  const breakDecrement = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1)
+    }
+  }
+
+  const sessionIncrement = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1)
+    }
+  }
+
+  const sessionDecrement = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1)
+    }
+  }
+
+  const formatTimeLeft = () => {
+    let minutes = Math.floor(timeLeft / 60)
+    let seconds = Math.floor(timeLeft % 60)
+    if (minutes < 10) {minutes = "0" + minutes}
+    if (seconds < 10) {seconds = "0" + seconds}
+
+    let formattedSeconds = minutes + ":" + seconds
+    return formattedSeconds
+  }
+
+  let timeLeftFormatted = formatTimeLeft()
+
   useEffect(() => {
     let timeout
 
@@ -38,36 +74,24 @@ function App() {
 
   return (
     <div id="clock">
-      <BreakButtonGroup breakLength={breakLength}/>
-      <SessionButtonGroup sessionLength={sessionLength}/>
+      <div id="break-label" className="button-group">
+        <p>Break Length</p>
+        <button id="break-increment" onClick={breakIncrement}>+</button>
+        <p id="break-length">{breakLength}</p>
+        <button id="break-decrement" onClick={breakDecrement}>-</button>
+      </div>
+      <div id="session-label" className="button-group">
+        <p>Session Length</p>
+        <button id="session-increment" onClick={sessionIncrement}>+</button>
+        <p id="session-length">{sessionLength}</p>
+        <button id="session-decrement" onClick={sessionDecrement}>-</button>
+      </div>
       <div id="clock-wrapper">
         <p id="timer-label">Session</p>
-        <p id="time-left">{timeLeft}</p>
+        <p id="time-left">{timeLeftFormatted}</p>
         <button id="start_stop" onClick={startStopClickHandler}>>||</button>
         <button id="reset" onClick={resetClickHandler}>reset</button>
       </div>
-    </div>
-  )
-}
-
-function BreakButtonGroup(props) {
-  return (
-    <div id="break-label" className="button-group">
-      <p>Break Length</p>
-      <button id="break-increment">+</button>
-      <p id="break-length">{props.breakLength}</p>
-      <button id="break-decrement">-</button>
-    </div>
-  )
-}
-
-function SessionButtonGroup(props) {
-  return (
-    <div id="session-label" className="button-group">
-      <p>Session Length</p>
-      <button id="session-increment">+</button>
-      <p id="session-length">{props.sessionLength}</p>
-      <button id="session-decrement">-</button>
     </div>
   )
 }
